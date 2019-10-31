00650a2237f0918eab1fad5b7ff1a24f9b1f871f
Mihika <mihika.shilpi@me.com>
2019-10-31 21:06:02 +0100

    start p2

---
+++ p2/src/scripts.js
@@ -0,0 +1,27 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        'result': null,
+        'results': [],
+        'counter': 0,
+        'coin': null,
+        'attempt': null,
+        'score': 0,
+    },
+    methods: {
+        check: function (guess) {
+            this.coin = Math.floor(Math.random() * 5 + 1);
+            this.attempt = guess;
+            this.result = this.coin == this.attempt
+            this.results.push(this.result);
+            this.counter++;
+            this.score = this.results.reduce((a, b) => a + b, 0);
+        },
+        retry: function() {
+            this.coin = null;
+            this.attempt = null;
+            this.result = null;
+        },
+}
+
+})
